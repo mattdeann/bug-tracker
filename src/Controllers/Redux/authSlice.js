a332ab3d1d67a08e62fc8368c09600a268501b24
mattdean <matthewallandean@gmail.com>
2021-03-15 11:32:22 -0600

    Finish auth slice

--- src/Controllers/Redux/authSlice.js
+++ src/Controllers/Redux/authSlice.js
@@ -8,13 +8,20 @@ const slice = createSlice({
   },
   reducers: {
     signIn: (state, action) => {
-
+      const {name, password} = action.payload;
+      state.loggedIn = true;
+      state.admin = true;
     },
     signOut: (state) => {
-
+      state.loggedIn = false;
+      state.admin = false;
     },
     createUser: (state, action) => {
-      
+
     }
   }
 })
+
+export default slice.reducer;
+
+export const {signIn, signOut, createUser} = slice.actions;
